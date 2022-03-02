3c2afd6ad95b402ab94e58d6625ef7a4fddbba6f
Dmitry Rodin <dmrodin@bk.ru>
2022-03-02 11:28:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lesson_2_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            ushort value;
+            Console.WriteLine("Введите трехзначное число");
+            try
+            {
+                value = (ushort)Convert.ToUInt16(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Введено не корректное значение");
+                throw;
+            }
+
+            if(value < 100 || value > 999) 
+            {
+                Console.WriteLine("Введено не трехзначное число");
+                return;
+            }
+            
+            ushort firstNumber, midleNumber, lastNumber;
+
+            firstNumber = (ushort)(value / 100);
+            midleNumber = (ushort)(value / 10 % 10);
+            lastNumber = (ushort)(value % 10);
+
+            Console.WriteLine(firstNumber >= midleNumber && midleNumber > lastNumber);
+        }
+    }
+}
